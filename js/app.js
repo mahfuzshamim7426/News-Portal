0e60fe4df59edc4c4b64812c6539087378c635b7
mahfuzshamim7426 <mahfuzshamim7426@gmail.com>
2022-09-03 00:47:01 +0600

    blog page

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-const loadCategory = () => {
+const loadCategoryMenu = () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayCategory(data.data.news_category))
+        .then(data => displayCategoryMenu(data.data.news_category))
 }
-const displayCategory = (categories) => {
+const displayCategoryMenu = (categories) => {
     // console.log('categories', categories)
-    loadId(categories[0].category_id)
+    loadCategoryId(categories[0].category_id)
     console.log('categories[0].category_id', categories[0].category_id)
     for (const category of categories) {
         // console.log(category);
         const categoryContainer = document.getElementById('category-container');
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
-        <div onclick=loadId('${category.category_id}') >
+        <div onclick=loadCategoryId('${category.category_id}') >
            <h5 class="category-item">${category.category_name}</h5>
         </div>
             `
         categoryContainer.appendChild(categoryDiv);
     }
 }
-const loadId = (id) => {
+const loadCategoryId = (id) => {
     // console.log('id', id)
     const idUrl = `https://openapi.programming-hero.com/api/news/category/${id} `
     fetch(idUrl)
         .then(res => res.json())
         .then(data => {
-            // console.log('data', data)
+            console.log('data', data)
 
             displayId(data.data)
         })
@@ -39,16 +39,31 @@ const displayId = (data) => {
         // console.log(categoryUrl)
         const categoryDiv = document.createElement('div')
         categoryDiv.innerHTML = `
-        <img src="${categoryUrl.image_url}" class="card-img-top" alt="...">
-       <div class="card-body">
-           <h5 class="card-title">Card title: ${categoryUrl.title}</h5>
-           <p class="card-text">${categoryUrl.details.slice(0, 200)}</p>
-           <a href="#" class="btn btn-primary">Go somewhere</a>
-       </div>
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="${categoryUrl.image_url}" class="img-fluid rounded-start  m-2" alt="...">
+
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body  mx-2 px-4">
+                        <h5 class="card-title">${categoryUrl.title}</h5>
+                        <p class="card-text mt-2">${categoryUrl.details.slice(0, 200)}</p>
+                        <div class="d-flex justify-content-around mt-2">
+                        <p class="card-title ">
+                        <img src="${categoryUrl.author.img}" class="img-fluid rounded-start  m-2 " style="height: 40px; width: 40px; border-radius: 155px;" alt="..."> ${categoryUrl.author.name}</p>
+                        <p class="card-text mt-3">${categoryUrl.total_view}</p>
+                        <p class="card-text mt-3">More > ${categoryUrl.rating.number}</p>
+
+                        </div>
+
+                        
+                        </div>
+                    </div>
+                </div>
        `
         displayNews.appendChild(categoryDiv);
     }
 
 }
-loadId();
-loadCategory();
+loadCategoryId();
+loadCategoryMenu();
